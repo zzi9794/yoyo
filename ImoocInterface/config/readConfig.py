288f4be725d7e1249af6cc460bd777a4f2b965cf
zzi9794 <zzi9794>
2019-04-18 19:30:21 +0800

    接口自动化

---
+++ ImoocInterface/config/readConfig.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+import os
+import configparser
+
+cur_path = os.path.dirname(os.path.realpath(__file__))
+configPath = os.path.join(cur_path, "cfg.ini")
+conf = configparser.ConfigParser()
+conf.read(configPath)
+
+smtp_server = conf.get("email", "smtp_server")
+
+sender = conf.get("email", "sender")
+
+psw = conf.get("email", "psw")
+
+receiver = conf.get("email", "receiver")
+
+port = conf.get("email", "port")
